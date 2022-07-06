45afd2e73b007ac3c46c572e6f5a5a5a597d97d5
Santiago Cardona <ssuarez@lean-tech.io>
2022-07-05 23:24:43 -0500

    PrimateData MathOperation

---
+++ Day 2/Primitive_data.py
@@ -0,0 +1,30 @@
+# print(len("Hola"))
+#
+# #String
+#
+# print("HOla"[-1])
+#
+# #Integer
+#
+# print(3.59)
+
+
+# num_char = len(input("What is your name? "))
+#
+# new_num_chat = str(num_char)
+#
+# print(f"Your name has {num_char} characters")
+#
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+result = first_digit + second_digit
+
+print(type(first_digit))
+print(result)
+
